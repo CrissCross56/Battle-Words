a20edd7c64dbd5af948fa4f82b7aa39503ff8cca
CrissCross56 <grant.terdoslavich@gmail.com>
2026-08-10 10:36:01 -0400

    finished up the barebones forms that we will be using

--- client/src/pages/JoinLobby.tsx
+++ client/src/pages/JoinLobby.tsx
@@ -1,9 +1,41 @@
 import {IonPage, IonTitle} from '@ionic/react';
+import { useForm } from 'react-hook-form';
+
 
 const JoinLobby: React.FC = () => {
+    const{
+        register,
+        handleSubmit,
+        reset,
+        watch,
+        formState: {errors, isSubmitting},
+    } = useForm({
+        defaultValues: {
+            userName: "",
+            lobbyCode: "",
+        }
+    })
+
+    const onSubmit = async (data: any) => {
+        //replace with tanstack query to send data to backend and get response
+        console.log(data);
+        reset();
+    }
+    
     return (
         <IonPage>
             <IonTitle>Join Lobby</IonTitle>
+            
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label> Username: </label>
+                <input {...register("userName")} />
+                <label> Lobby Code: </label>
+                <input {...register("lobbyCode")} />
+                <button type="submit" disabled={isSubmitting}>
+                    Join Lobby
+                </button>
+            </form>
+
         </IonPage>
     )
 }
--- client/src/pages/MakeLobby.tsx
+++ client/src/pages/MakeLobby.tsx
@@ -1,9 +1,37 @@
-import {IonPage, IonTitle, IonButton} from '@ionic/react';
-
+import {IonPage, IonTitle, IonButton, IonHeader} from '@ionic/react';
+import { useForm } from 'react-hook-form';
 const MakeLobby: React.FC = () => {
+    const{
+        register,
+        handleSubmit,
+        reset,
+        watch,
+        formState: {errors, isSubmitting},
+
+    } = useForm({
+        defaultValues: {
+            userName: "",
+        }
+    })
+
+    const onSubmit = async (data: any) =>{
+        //replace with tanstack query to send data to backend and get response
+        console.log(data);
+        reset();
+    }
+
     return (
         <IonPage>
-            <IonTitle>Make Lobby</IonTitle>
+            <IonTitle><IonHeader>Make Lobby</IonHeader></IonTitle>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label> Username: </label>
+                <input {...register("userName")} />
+                <button type="submit" disabled={isSubmitting}>
+                    Make Lobby
+                </button>
+            </form>
+
             <IonButton routerLink="/game-lobby">Go to Game Lobby</IonButton>
         </IonPage>
     )
