41bcb186690079b6f704835e9862681bfae78b36
Julio A. Alvarez <julioalvarez709@gmail.com>
2026-08-17 02:55:35 -0400

    feat(frontend): add :roomCode routes to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,41 +3,28 @@ import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 
 import Home from './pages/Home';
+import JoinLobby from './pages/JoinLobby';
+import MakeLobby from './pages/MakeLobby';
+import GameLobby from './pages/GameLobby';
+import HowToPlay from './pages/HowToPlay';
+import Game from './pages/Game';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import JoinLobby from './pages/JoinLobby';
-import MakeLobby from './pages/MakeLobby';
-import GameLobby from './pages/GameLobby';
-import HowToPlay from './pages/HowToPlay';
-import Game from './pages/Game';
+
 setupIonicReact();
 
 const App: React.FC = () => (
@@ -56,13 +43,13 @@ const App: React.FC = () => (
         <Route exact path="/make-lobby">
           <MakeLobby />
         </Route>
-        <Route exact path="/game-lobby">
+        <Route exact path="/game-lobby/:roomCode">
           <GameLobby />
         </Route>
         <Route exact path="/how-to-play">
           <HowToPlay />
         </Route>
-        <Route exact path="/Game">
+        <Route exact path="/game/:roomCode">
           <Game />
         </Route>
       </IonRouterOutlet>
